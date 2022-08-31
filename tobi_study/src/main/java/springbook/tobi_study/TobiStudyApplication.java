e29092dd13bfe000d07d96d3ddf9e6618c8b8229
inuit57 <inuit57@naver.com>
2022-08-31 17:24:44 +0900

    Initial commit

---
+++ tobi_study/src/main/java/springbook/tobi_study/TobiStudyApplication.java
@@ -0,0 +1,13 @@
+package springbook.tobi_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobiStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobiStudyApplication.class, args);
+	}
+
+}
---
+++ tobi_study/src/test/java/springbook/tobi_study/TobiStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package springbook.tobi_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobiStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
