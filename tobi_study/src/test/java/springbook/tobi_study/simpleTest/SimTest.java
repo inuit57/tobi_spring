cc1157174a71af052c0ab58b19744817ce3db904
inuit57 <inuit57@naver.com>
2022-09-01 15:15:30 +0900

    test : List 객체를 get으로 함부로 받아오게 해서는 안된다.
    원본에 영향을 줄 수 있게 된다.

---
+++ tobi_study/src/test/java/springbook/tobi_study/simpleTest/SimTest.java
@@ -0,0 +1,43 @@
+package springbook.tobi_study.simpleTest;
+
+import lombok.Getter;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SimTest {
+
+    @Getter
+    static class TestList{
+        private List<String> strings = new ArrayList<>();
+
+        public TestList(List<String> names){
+            this.strings = new ArrayList<String>(names);
+        }
+    }
+
+    @Test
+    void list_copy(){
+
+        List<String> stringList = new ArrayList<>();
+        stringList.add("abc");
+        stringList.add("cdb");
+
+        TestList tl = new TestList(stringList);
+
+        stringList.clear(); // 복사한 개체라서 비워도 문제가 없다.
+        assertThat(tl.getStrings()).contains("abc");
+
+        List<String> copyList = tl.getStrings();
+        copyList.clear(); // 복사가 아니라서 원본에 영향을 준다.
+
+        // 원본이 손상되었다.
+        assertThat(tl.getStrings()).doesNotContain("abc");
+
+
+    }
+}
